1a3b2df9cd1a9a9c6be4bf8ada523953672567f9
drshnsn <darsh.sen@gmail.com>
2017-03-19 11:40:07 -0400

    ajax call

--- src/app.js
+++ src/app.js
@@ -1,1 +1,47 @@
-console.log("working")
+import React from 'react';
+import ReactDOM from 'react-dom';
+import {ajax} from 'jquery';
+import sweetalert from 'sweetalert';
+
+const config = {
+    apiKey: "AIzaSyBONPu54xv5ss_aGeSe7P1zNR9eiBOS1k0",
+    authDomain: "noted-d6e79.firebaseapp.com",
+    databaseURL: "https://noted-d6e79.firebaseio.com",
+    storageBucket: "noted-d6e79.appspot.com",
+    messagingSenderId: "528891283267"
+  };
+  firebase.initializeApp(config);
+
+  	componentDidMount() {
+  		ajax({
+  			url: "https://itunes.apple.com/search",
+  			method: 'GET',
+  			dataType: 'jsonp',
+  			data: {
+  				term: "health and fitness",
+  				country: "US",
+          media: "podcast",
+  			}
+  		}).then(function(res) {
+  			   res.map((podcastDetails) => {
+            <h2>{podcastDetails.collectionName}</h2>
+            <img src="{podcastDetails.artworkUrl60}" alt=""/>
+
+           })
+        // if (primaryGnereName ==== podcastGenre) {
+        //   Comedy = comedy 
+             collectionName === podcastName
+             artwork
+        // }
+  		})
+  	}
+  }
+
+    class App extends React.Component {
+    render() {
+      return (
+          <div></div>
+      )
+    }
+
+  ReactDOM.render(<App />, document.getElementById('app'));
