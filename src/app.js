d5385b3fcec1c3624a8c80f674e8670230357a58
drshnsn <darsh.sen@gmail.com>
2017-03-20 17:57:53 -0400

    Sass & Styles update

--- src/app.js
+++ src/app.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import {ajax} from 'jquery';
 import sweetalert from 'sweetalert';
+import Inputs from './components/Inputs.js';
 
 const config = {
     apiKey: "AIzaSyBONPu54xv5ss_aGeSe7P1zNR9eiBOS1k0",
@@ -16,54 +17,83 @@ class App extends React.Component {
     constructor() {
       super();
       this.state = {
-        podcasts: []
+        podcasts: [],
+        term: ''
       }
+      this.handleClick = this.handleClick.bind(this);
     }
-    handleClick() {
-      this.setState({
-
-      });
-    }
-    componentDidMount() {
+    handleClick(e) {
+   
       ajax({
         url: 'https://itunes.apple.com/search',
         method: 'GET',
         dataType: 'jsonp',
         data: {
-          term: 'Comedy',
+          term: e.target.value,
           country: 'US',
           media: 'podcast',
           entity: 'podcast',
-          genreIndex: 'Comedy'
-
+          attribute: 'descriptionTerm'
         }
       }).then((podcastList) => {
         console.log(podcastList.results)
         this.setState({podcasts: podcastList.results});
       });
     }
-        render() {
+    render() {
       return (
           <div>
-            <header>
+            {/*<header>
               <h1>Pod Save the Day</h1>
-            </header>
+            </header>*/}
+            <div className="genreChoice">
+              <h2>Choose your own Podventure:</h2>
+                <input type="radio" id="Comedy" name="genreType" value="Comedy" onClick={this.handleClick} />
+                  <label htmlFor="Comedy">
+                    <p>Comedy</p>
+                  </label>
+                <input type="radio" id="Politics" name="genreType" value="Politics" onClick={this.handleClick} />
+                  <label htmlFor="Politics">
+                    <p>Politics</p>
+                  </label>
+                <input type="radio" id="Technology" name="genreType" value="Technology" onClick={this.handleClick} />
+                  <label htmlFor="Technology">
+                    <p>Technology</p>
+                  </label>
+                <input type="radio" id="Food" name="genreType" value="Food" onClick={this.handleClick} />
+                  <label htmlFor="Food">
+                    <p>Food</p>
+                  </label>
+                <input type="radio" id="Music" name="genreType" value="Music" onClick={this.handleClick} />
+                  <label htmlFor="Music">
+                    <p>Music</p>
+                  </label>
+                <input type="radio" id="Science" name="genreType" value="Science" onClick={this.handleClick} />
+                  <label htmlFor="Science">
+                    <p>Science</p>
+                  </label>
+                <input type="radio" id="Arts" name="genreType" value="Arts" onClick={this.handleClick} />
+                  <label htmlFor="Arts">
+                    <p>Arts</p>
+                  </label>
+            </div>
             <div className="podcastOptions">
               {this.state.podcasts.map((podcast, i) => {
-                 if (podcast.primaryGenreName === "Comedy") {
-                return (
-                 
+               
+                  return (
                     <div className="podcastOptionsList" key={`podcast-${i}`}>
                         <h4>{podcast.collectionName}</h4>
                         <img src={podcast.artworkUrl100} alt=""/>
-                        <p></p>
+                        <button><a href={podcast.trackViewUrl}>CAST OFF</a></button>
                     </div>
-                  )}
+                  )
+                // }
               })}
+              <button>Load More</button>
             </div>
           </div>
         )
     }
-    }
+}
 
   ReactDOM.render(<App />, document.getElementById('app'));
