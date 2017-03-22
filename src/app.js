ba1b51c32a9d0fc82f59248f8514bfbc678f53b7
drshnsn <darsh.sen@gmail.com>
2017-03-22 08:49:04 -0400

    firebase & style updates

--- src/app.js
+++ src/app.js
@@ -63,62 +63,77 @@ class App extends React.Component {
     render() {
       return (
           <div>
-            {/*<header>
-              <h1>Pod Save the Day</h1>
-            </header>*/}
+            <header>
+              <h1>The <br/>Podfather</h1>
+              <div>
+                <h4>A Podcast Search Tool</h4>
+                <button>Let's Cast Off</button>
+              </div>
+            </header>
             <div className="genreChoice">
               <h2>Choose your own Podventure:</h2>
-                <input type="radio" id="Comedy" name="genreType" value="Comedy" onClick={this.handleClick} />
-                  <label htmlFor="Comedy">
-                    <p>Comedy</p>
-                  </label>
-                <input type="radio" id="Politics" name="genreType" value="Politics" onClick={this.handleClick} />
-                  <label htmlFor="Politics">
-                    <p>Politics</p>
-                  </label>
-                <input type="radio" id="Technology" name="genreType" value="Technology" onClick={this.handleClick} />
-                  <label htmlFor="Technology">
-                    <p>Technology</p>
-                  </label>
-                <input type="radio" id="Food" name="genreType" value="Food" onClick={this.handleClick} />
-                  <label htmlFor="Food">
-                    <p>Food</p>
-                  </label>
-                <input type="radio" id="Music" name="genreType" value="Music" onClick={this.handleClick} />
-                  <label htmlFor="Music">
-                    <p>Music</p>
-                  </label>
-                <input type="radio" id="Science" name="genreType" value="Science" onClick={this.handleClick} />
-                  <label htmlFor="Science">
-                    <p>Science</p>
-                  </label>
-                <input type="radio" id="Arts" name="genreType" value="Arts" onClick={this.handleClick} />
-                  <label htmlFor="Arts">
-                    <p>Arts</p>
-                  </label>
+                <div className="inputs">
+                  <input type="radio" id="Comedy" name="genreType" value="Comedy" onClick={this.handleClick} />
+                    <label htmlFor="Comedy">
+                      <img src="../Assets/happy.png" alt=""/>
+                      <p>Comedy</p>
+                    </label>
+                  <input type="radio" id="Politics" name="genreType" value="Politics" onClick={this.handleClick} />
+                    <label htmlFor="Politics">
+                      <img src="../Assets/debate.png" alt=""/>
+                      <p>Politics</p>
+                    </label>
+                  <input type="radio" id="Technology" name="genreType" value="Technology" onClick={this.handleClick} />
+                    <label htmlFor="Technology">
+                      <img src="../Assets/smartphone.png" alt=""/>
+                      <p>Technology</p>
+                    </label>
+                  <input type="radio" id="Food" name="genreType" value="Food" onClick={this.handleClick} />
+                    <label htmlFor="Food">
+                      <img src="../Assets/stand.png" alt=""/>
+                      <p>Food</p>
+                    </label>
+                  <input type="radio" id="Music" name="genreType" value="Music" onClick={this.handleClick} />
+                    <label htmlFor="Music">
+                      <img src="../Assets/music-player.png" alt=""/>
+                      <p>Music</p>
+                    </label>
+                  <input type="radio" id="Science" name="genreType" value="Science" onClick={this.handleClick} />
+                    <label htmlFor="Science">
+                      <img src="../Assets/flask.png" alt=""/>
+                      <p>Science</p>
+                    </label>
+                  <input type="radio" id="Arts" name="genreType" value="Arts" onClick={this.handleClick} />
+                    <label htmlFor="Arts">
+                      <img src="../Assets/palette.png" alt=""/>
+                      <p>Arts</p>
+                    </label>
+                  </div>
             </div>
-            <div className="podcastOptions">
-              {this.state.podcasts.map((podcast, i) => {
-               
-                  return (
-                    <div className="podcastOptionsList" key={`podcast-${i}`}>
-                        <h4>{podcast.collectionName}</h4>
-                        <img src={podcast.artworkUrl100} alt=""/>
-                        <button><a href={podcast.trackViewUrl}>More Info</a></button>
-                        <button onClick={(e) => this.addItem(e, podcast.collectionName, podcast.artworkUrl100)}>Add to Collection</button>
-                    </div>
-                    
-                  )
-                // }
-              })}
-              
-            </div>
-              <div className="podcastChoices">
-              <ul>
-                {this.state.items.map((item, i) => {
-                  return <li>{item.name}<img src={item.img} alt="" /><i onClick={() => this.removeItem(item.key)} className="fa fa-times" aria-hidden="true"></i></li>
+            <div className="podcastDisplay">
+              <div className="podcastOptions">
+                {this.state.podcasts.map((podcast, i) => {
+                    return (
+                      <div className="podcastOptionsList" key={`podcast-${i}`}>
+                          <h4>{podcast.collectionName}</h4>
+                          <img src={podcast.artworkUrl100} alt=""/>
+                          <button><a href={podcast.trackViewUrl}>More Info</a></button>
+                          <button onClick={(e) => this.addItem(e, podcast.collectionName, podcast.artworkUrl100)}>Add to Collection</button>
+                      </div>
+                      
+                    )
+                  // }
                 })}
-              </ul>
+                
+              </div>
+                <div className="podcastChoices">
+                <ul>
+                <h2>Your Picks</h2>
+                  {this.state.items.map((item, i) => {
+                    return <li>{item.name}<img src={item.img} alt="" /><i onClick={() => this.removeItem(item.key)} className="fa fa-times" aria-hidden="true"></i></li>
+                  })}
+                </ul>
+              </div>
             </div>
           </div>
         )
